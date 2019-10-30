29b7a4b8c227f87db4a6862f1abd17ce29ed27db
hjwblog.com <yysz169hjw@126.com>
2019-10-30 23:39:30 +0800

    feat: init

---
+++ src/main/java/edu/hfut/hawk_eye/HawkEyeApplication.java
@@ -0,0 +1,13 @@
+package edu.hfut.hawk_eye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkEyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawkEyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hfut/hawk_eye/HawkEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hfut.hawk_eye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawkEyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
